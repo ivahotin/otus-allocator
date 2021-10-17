e30845c7ebc0821b4a78abb491c1bc35e59a9fa3
Igor <ivahotin@gmail.com>
2021-10-17 15:33:30 +0300

    Allocator and custom list

---
+++ src/list.h
@@ -0,0 +1,86 @@
+#pragma once
+
+#include <memory>
+
+namespace custom {
+
+template <typename T, typename allocator = std::allocator<T>>
+class list {
+public:
+    struct node {
+        node* next = nullptr;
+        T value;
+
+        node() {}
+        explicit node(const T& val, node* next) : next(next), value(std::move(val)) {}
+    };
+
+    class iterator {
+        friend class list<T, allocator>;
+
+        iterator(node* n_) : node_(n_) {}
+
+        public:
+        iterator& operator++()
+        {
+            node_ = node_ -> next;
+            return *this;
+        }
+
+        const T& operator*() const
+        {
+            return node_->value;
+        }
+
+        bool operator!=(const iterator& other)
+        {
+            return node_ != other.node_;
+        }
+
+    private:
+        node* node_;
+    };
+
+    using node_allocator = typename allocator::template rebind<node>::other;
+
+    list() = default;
+    ~list() {
+        while (head != nullptr) {
+            auto tmp = head;
+            head = head -> next;
+            node_alloc.destroy(tmp);
+            node_alloc.deallocate(tmp, 1);
+        }
+    };
+
+    iterator begin() const {
+        return {head};
+    }
+
+    iterator end() const {
+        return {nullptr};
+    }
+
+    void add(const T& val) {
+        node* n = node_alloc.allocate(1);
+        node_alloc.construct(n, std::move(val), head);
+        head = n;
+        size_++;
+    }
+
+    std::size_t size() const {
+        return size_;
+    }
+
+    bool empty() const {
+        return size() == 0;
+    }
+
+private:
+    node*       head  = nullptr;
+    std::size_t size_ = 0;
+
+    node_allocator node_alloc;
+};
+
+}
